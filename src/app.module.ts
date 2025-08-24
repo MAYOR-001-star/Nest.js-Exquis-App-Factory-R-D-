618a3379bfc52fc5456dfb7a6a000e63adadcc3e
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-08-24 23:26:17 +0100

    learning modules especially dynamic ones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { GreetingModule } from './greeting/greeting.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), GreetingModule],
   controllers: [AppController],
   providers: [],
 })
